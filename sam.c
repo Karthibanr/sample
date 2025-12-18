e72bdf33b7ebd57fee6788bdfe83e47289f15ec3
Karthibanr <karthibanlink@gmail.com>
2025-12-18 14:46:27 +0530

    Fix typo in sample C program output

--- sam.c
+++ sam.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
     printf("Hello, World!\n");
-    printf("This is a sample C prhrthrehrehreogram.\n");
+    printf("This is a sample C program.\n");
     return 0;
 }
